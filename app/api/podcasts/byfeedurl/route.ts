22929b29f1a3e7f5b8c31a93a60ca74c82efc62b
sinchang <sinchangwen@gmail.com>
2023-08-05 17:06:43 +0800

    update api

---
+++ app/api/podcasts/byfeedurl/route.ts
@@ -0,0 +1,30 @@
+import { unstable_cache } from 'next/cache'
+import { NextResponse } from 'next/server'
+import PodcastIndexClient from 'podcast-index-client'
+import { parse } from 'rss-to-json'
+
+const client = new PodcastIndexClient({
+  key: process.env.PODCAST_INDEX_API_KEY,
+  secret: process.env.PODCAST_INDEX_API_SECRET,
+  disableAnalytics: true,
+})
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const url = searchParams.get('url')
+
+  if (!url) return new Response('feed url is required', { status: 400 })
+
+  const cacheData = await unstable_cache(
+    async () => {
+      const { feed } = await client.podcastByUrl(url)
+      return feed
+    },
+    [url],
+    {
+      revalidate: 86400,
+    }
+  )()
+
+  return NextResponse.json({ data: cacheData })
+}
